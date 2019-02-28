883c755e6e541b4f7ef5d0f6bf049f4d5d5e6f0c
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-02-27 17:30:09 -0700

    base game works in console

---
+++ assets/javascript/game.js
@@ -0,0 +1,70 @@
+var targetScore = 0;
+var currentScore = 0;
+
+var diamondValue = 0;
+var rubyValue = 0;
+var topazValue = 0;
+var emeraldValue = 0;
+
+var wins = 0;
+var losses = 0;
+
+function randomTargetScore() {
+    targetScore = Math.floor(Math.random() * 100) + 19;
+
+}
+
+function randomGemValue() {
+    var value = Math.floor(Math.random() * 12) + 1;
+    return value;
+
+}
+
+
+function reset() {
+
+    randomTargetScore();
+    currentScore = 0;
+
+    diamondValue = randomGemValue();
+    $("#diamond").attr("data-gemValue", diamondValue);
+    rubyValue = randomGemValue();
+    $("#ruby").attr("data-gemValue", rubyValue);
+    topazValue = randomGemValue();
+    $("#topaz").attr("data-gemValue", topazValue);
+    emeraldValue = randomGemValue();
+    $("#emerald").attr("data-gemValue", emeraldValue);
+
+    console.log("target Score: " + targetScore);
+    console.log("current Score: " + currentScore);
+    console.log("diaVal: " + diamondValue)
+    console.log("diaVal: " + $("#diamond").attr("data-gemValue"));
+    console.log("rubVal: " + rubyValue)
+    console.log("topVal: " + topazValue)
+    console.log("emeVal: " + emeraldValue)
+
+}
+
+reset();
+
+$(".crystal-image").on("click", function () {
+    var gemValue = ($(this).attr("data-gemValue"));
+    gemValue = parseInt(gemValue);
+
+    currentScore += gemValue;
+    console.log(currentScore);
+    if (currentScore === targetScore) {
+        console.log("You Win!")
+        wins++;
+        console.log("Wins: " + wins);
+        console.log("Losses: " + losses);
+        reset();
+
+    } else if (currentScore > targetScore) {
+        console.log("You Lose!")
+        losses++;
+        console.log("Wins: " + wins);
+        console.log("Losses: " + losses);
+        reset();
+    }
+})
